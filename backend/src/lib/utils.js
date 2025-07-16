81194216636c703402996cf3d5e793504b58fd00
WebHault108 <biswasritam5.13@gmail.com>
2025-07-16 11:55:02 +0530

    add lax in  utils

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "None", // ✅ allow cross-origin cookie
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "strict",
     secure: true, // ✅ must be true when sameSite is "None"
   });
 
