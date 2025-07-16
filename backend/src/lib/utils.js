dc22d07b3f122b1b3065541a3dbec52c585fe357
WebHault108 <biswasritam5.13@gmail.com>
2025-07-16 11:27:48 +0530

    utis cookie same site changes

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,20 +1,19 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 export const generateToken = (userId, res) => {
-    //create token
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    })
-    console.log("Cookies generated");
-    
+  //create token
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  console.log("Cookies generated");
 
-    //increse security for cookie
-    res.cookie("jwt", token, {
-        maxAge: 7*24*60*60*1000, // mili seconds support only
-        httpOnly: true,
-        sameSite: process.env.NODE_ENV === "development" ? "lax" : "strict",
-        secure: process.env.NODE_ENV !== "development"
-    })
+  //increse security for cookie
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "None", // ✅ allow cross-origin cookie
+    secure: true, // ✅ must be true when sameSite is "None"
+  });
 
-    //return token
-    return token
-}
+  //return token
+  return token;
+};
